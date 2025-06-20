4382a0ccf2ba90d603b2d25bf8eadf718be3ce74
Luar <luarjs@gmail.com>
2025-06-20 14:43:12 +0200

    feat: add tech and change cv

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -52,7 +52,7 @@ export const SocialMedia = () => {
         <li style={{ "--delay": "300ms" } as React.CSSProperties}>
           <a
             className="block social-media-icon"
-            href="/documents/Luar-Jaen-Sole-CV.pdf"
+            href="/documents/luar-jaen-sole-cv.pdf"
             download
             aria-label="Descargar CV"
             title="Descargar CV"

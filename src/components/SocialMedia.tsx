f7e4e3b595a0aeb051c9156fc8eddc6f72261d21
Luar <luarjs@gmail.com>
2025-06-23 13:11:21 +0200

    fix: change a title

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -53,13 +53,13 @@ const SocialMedia = () => {
           <a
             className="block social-media-icon"
             href="/documents/luar-jaen-sole-cv.pdf"
-            aria-label="Descargar PDF"
-            title="Descargar PDF"
+            aria-label="Descargar CV"
+            title="Descargar CV"
             download
           >
             <img
               src="/cv.svg"
-              alt="Ícono de CV"
+              alt="Icono de CV"
               className="h-6 w-6 transition-transform duration-200 ease-in-out scale-125 hover:scale-150 drop-shadow-[0_2px_1px_#4723a5] hover:drop-shadow-[0_2px_2px_#4723a5] brightness-0 invert object-contain"
               width="24"
               height="24"

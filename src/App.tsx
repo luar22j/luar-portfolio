a307dded138a2a6de6e73f51f4433d5c2183149d
Luar <luarjs@gmail.com>
2025-06-23 13:02:56 +0200

    chore: delete unused routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,25 @@ import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import About from "./components/About";
 import Footer from "./components/Footer";
-import NotFound from "./components/NotFound";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Background>
-              <div className="flex flex-col items-center min-h-screen lg:px-[100px]">
-                <div className="flex flex-col h-screen justify-center md:gap-[200px] gap-[100px]">
-                  <Profile />
-                  <SocialMedia />
-                </div>
-                <div className="flex flex-col items-center justify-center md:gap-[200px] gap-[100px]">
-                  <div className="flex lg:flex-row flex-col lg:gap-8 md:gap-[200px] gap-[100px]">
-                    <About />
-                    <Experience />
-                  </div>
-                  <Projects />
-                  <Footer />
-                </div>
-              </div>
-            </Background>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Background>
+      <div className="flex flex-col items-center min-h-screen lg:px-[100px]">
+        <div className="flex flex-col h-screen justify-center md:gap-[200px] gap-[100px]">
+          <Profile />
+          <SocialMedia />
+        </div>
+        <div className="flex flex-col items-center justify-center md:gap-[200px] gap-[100px]">
+          <div className="flex lg:flex-row flex-col lg:gap-8 md:gap-[200px] gap-[100px]">
+            <About />
+            <Experience />
+          </div>
+          <Projects />
+          <Footer />
+        </div>
+      </div>
+    </Background>
   );
 }
 

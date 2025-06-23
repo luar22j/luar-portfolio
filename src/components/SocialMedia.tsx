af0b2e1748c75c112750a9ccdf270b403d890f07
Luar <luarjs@gmail.com>
2025-06-23 13:09:35 +0200

    fix: delete resume component

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,5 +1,4 @@
 import socialMediaData from "../../public/data/social-media.json";
-import Resume from "./Resume";
 
 const SocialMedia = () => {
   const svg = (
@@ -51,7 +50,21 @@ const SocialMedia = () => {
           );
         })}
         <li style={{ "--delay": "300ms" } as React.CSSProperties}>
-          <Resume />
+          <a
+            className="block social-media-icon"
+            href="/documents/luar-jaen-sole-cv.pdf"
+            aria-label="Descargar PDF"
+            title="Descargar PDF"
+            download
+          >
+            <img
+              src="/cv.svg"
+              alt="Ícono de CV"
+              className="h-6 w-6 transition-transform duration-200 ease-in-out scale-125 hover:scale-150 drop-shadow-[0_2px_1px_#4723a5] hover:drop-shadow-[0_2px_2px_#4723a5] brightness-0 invert object-contain"
+              width="24"
+              height="24"
+            />
+          </a>
         </li>
       </ul>
     </div>
